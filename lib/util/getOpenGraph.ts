39d0dbc66e79906ca270c7c5410756ab9bdae717
JaeSeoKim <devjaeseo@gmail.com>
2021-10-21 01:15:52 +0900

    :package: Change puppeteer to got (no dom rendering)

--- lib/util/getOpenGraph.ts
+++ lib/util/getOpenGraph.ts
@@ -6,10 +6,8 @@ import metascraperDescription from "metascraper-description"
 import metascraperImage from "metascraper-image"
 import metascraperLogo from "metascraper-logo"
 import metascraperLogoFavicon from "metascraper-logo-favicon"
-import puppeteer from "puppeteer-core"
-import chrome from "chrome-aws-lambda"
 import { PromiseType } from "utility-types"
-import { isDev } from "./config"
+import got from "got"
 
 const metaScraper = metascraper([
   metascraperUrl(),
@@ -20,57 +18,16 @@ const metaScraper = metascraper([
   metascraperLogo(),
   metascraperLogoFavicon(),
 ])
-
-const exePath =
-  process.platform === "win32"
-    ? "C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe"
-    : process.platform === "linux"
-    ? "/usr/bin/google-chrome"
-    : "/Applications/Google Chrome.app/Contents/MacOS/Google Chrome"
-
-interface Options {
-  args: string[]
-  executablePath: string
-  headless: boolean
-}
-
-export async function getPuppeterOption(isDev: boolean) {
-  let options: Options
-  if (isDev) {
-    options = {
-      args: [],
-      executablePath: exePath,
-      headless: true,
-    }
-  } else {
-    options = {
-      args: chrome.args,
-      executablePath: await chrome.executablePath,
-      headless: chrome.headless,
-    }
-  }
-  return options
-}
-
 export interface getOpenGraphProps {
   url: string
 }
 
 const getOpenGraph = async ({ url }: getOpenGraphProps) => {
-  const puppeteerOption = await getPuppeterOption(isDev)
-  const browser = await puppeteer.launch(puppeteerOption)
-  const page = await browser.newPage()
-  await page.goto(url, {
-    waitUntil: "load",
-  })
-
-  const html = await page.content()
-
-  await browser.close()
+  const { url: requestURL, body } = await got(url)
 
   const results = await metaScraper({
-    url: url,
-    html: html,
+    url: requestURL,
+    html: body,
   })
   return results
 }
